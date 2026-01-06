5fc32428e0a65149efaa214e84c5298f6d55d603
Bogdiyandrew <bpotocaruu@gmail.com>
2026-01-06 19:53:52 +0200

    reaL

--- app/portofoliu/page.tsx
+++ app/portofoliu/page.tsx
@@ -69,7 +69,7 @@ export default function PortofoliuPage() {
                   fill
                   className="object-cover group-hover:scale-110 transition-transform duration-700"
                 />
-                <div className="absolute inset-0 bg-gradient-to-t from-zinc-900/80 to-transparent opacity-60 group-hover:opacity-40 transition-opacity" />
+                <div className="absolute inset-0 bg-linear-to-t from-zinc-900/80 to-transparent opacity-60 group-hover:opacity-40 transition-opacity" />
               </div>
 
               <div className="p-8">
